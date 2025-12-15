93d3788634f21462bcb0f9df769d6ec4b9928289
yassinek <khafifiyassine333@gmail.com>
2025-12-15 21:29:11 +0100

    Fix: update vite base path for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ⚠️ INDISPENSABLE pour corriger la page blanche sur Netlify
+  base: '/', // Correction pour Netlify : chemin absolu
 })
