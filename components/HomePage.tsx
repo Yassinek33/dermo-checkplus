0ce0341ca9032ccd6585efac9473f709a9dcd306
yassinek <khafifiyassine333@gmail.com>
2026-02-24 00:43:46 +0100

    style: enable mobile visibility for clinical expertise section

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -134,8 +134,8 @@ const HomePage: React.FC<HomePageProps> = ({ config, onStart, onNavigate, user }
                 <AuthMarquee onNavigate={onNavigate} user={user} />
             </div>
 
-            {/* BRAND SHOWCASE - Clinical Style (Hidden on Mobile) */}
-            <section className="hidden md:block relative h-[60vh] md:h-[80vh] w-full overflow-hidden rounded-[3rem] border border-white/10 shadow-3xl bg-[#080809]">
+            {/* BRAND SHOWCASE - Clinical Style */}
+            <section className="relative h-[60vh] md:h-[80vh] w-full overflow-hidden rounded-3xl md:rounded-[3rem] border border-white/10 shadow-3xl bg-[#080809]">
                 <div className="absolute inset-0 z-0">
                     <img
                         src={language === 'fr' ? '/clinical-expertise-fr.png' : '/clinical-expertise-en.png'}
